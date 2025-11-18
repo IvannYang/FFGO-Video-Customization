1448dcaac5a7842550cae9900d5a1d2d09c36530
ZONGXIA LI <zongxiali@ZONGXIAs-MacBook-Pro.local>
2025-11-18 18:58:56 -0500

    modifying download script

--- download.sh
+++ download.sh
@@ -8,8 +8,8 @@ set -e  # Exit on any error
 python ./VideoX-Fun/examples/wan2.2/batch_predict-v1.py \
     --resolution 720x1080 \
     --model_name ./Models/Wan2.2-I2V-A14B \
-    --lora_low ./Models/Lora/10_LargeMixedDatset_wan_14bLow_f81_LongCaption_StartMatch_run_r128_a128_3obj_Longrun_B4/checkpoint-600-low.safetensors \
-    --lora_high ./Models/Lora/10_LargeMixedDatset_wan_14bHigh_f81_LongCaption_StartMatch_run_r128_a128_3obj_Longrun_B4/checkpoint-600-high.safetensors \
+    --lora_low ./Models/Lora/10_LargeMixedDatset_wan_14bLow_f81_LongCaption_StartMatch_run_r128_a128_3obj_Longrun_B4/checkpoint-600.safetensors \
+    --lora_high ./Models/Lora/10_LargeMixedDatset_wan_14bHigh_f81_LongCaption_StartMatch_run_r128_a128_3obj_Longrun_B4/checkpoint-600.safetensors \
     --config_path ./VideoX-Fun/config/wan2.2/wan_civitai_i2v.yaml \
     --data_csv ./Data/combined_first_frames/0-data.csv \
     --output_path ./output/ffgo_eval
